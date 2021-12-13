6e455d04d6c31a425f30c5f6d35921b5f5ed1557
kingfree <kingfree@toyama.moe>
2021-12-13 14:36:11 +0800

    Keep alives should be >= 5  secs

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -49,7 +49,7 @@ impl MqttClient {
     pub fn new(three: &ThreeTuple, info: &DeviceAuthInfo, instance: &MqttInstance) -> Result<Self> {
         let (host, port) = instance.url();
         let mut options = MqttOptions::new(&info.client_id, &host, port);
-        options.set_keep_alive(Duration::from_secs(1));
+        options.set_keep_alive(Duration::from_secs(10));
         options.set_credentials(&info.username, &info.password);
 
         Ok(Self {
